096bc2275eed2fb735fd1d841b6084d152019668
ramil reziapov <ramil6191@outlook.com>
2025-11-29 17:17:54 +0400

    Fix metadata title format in GamePage component

--- src/app/(main)/games/[gameId]/page.tsx
+++ src/app/(main)/games/[gameId]/page.tsx
@@ -21,7 +21,7 @@ export async function generateMetadata({ params }: GamePageProps) {
   }
 
   return {
-    title: `${game.name} | sci5th`,
+    title: `sci5th | ${game.name}`,
     description: game.description,
   };
 }
