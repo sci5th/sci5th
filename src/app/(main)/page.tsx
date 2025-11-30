b30522a3c28a5182a9863f47cf6d10530cc627aa
ramil reziapov <ramil6191@outlook.com>
2025-11-30 16:02:50 +0400

    Refactor HomePage component for improved grid spacing

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,7 +5,7 @@ import { games } from "@/config/games";
 export default function HomePage() {
   return (
     <div className="flex flex-1 px-4 py-8 md:px-8 lg:px-8">
-      <div className="grid gap-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+      <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {games.map((game) => (
           <div key={game.id} className="flex flex-col">
             <Link
